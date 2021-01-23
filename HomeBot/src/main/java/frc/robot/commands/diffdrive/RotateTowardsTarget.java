c261cd2f4a81e53a5852980bd5452ba73b83d55f
Peter Le <lep.23@stu.materdei.org>
2021-01-23 09:43:19 -0800

    Importing DiffDrive

---
+++ HomeBot/src/main/java/frc/robot/commands/diffdrive/RotateTowardsTarget.java
@@ -0,0 +1,56 @@
+
+package frc.robot.commands.diffdriver;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.consoles.Logger;
+import frc.robot.sensors.Limelight;
+import frc.robot.subsystems.DiffDriver;
+import frc.robot.BotSensors;
+
+// DiffDrive uses gyro and limelight to rotate to the target.
+public class RotateTowardsTarget extends CommandBase {
+
+    private DiffDriver m_diffDriver;
+    private double m_targetAngle = 0;
+
+    public RotateTowardsTarget(DiffDriver diffDriver) {
+        Logger.setup("Constructing Command: RotateTowardsTarget...");
+
+        // Add given subsystem requirements
+        m_diffDriver = diffDriver;
+        addRequirements(m_diffDriver);
+    }
+
+    @Override
+    public void initialize() {
+        // LEDs turned off in TurnOffLimelightArray command
+        Limelight.ledOn();
+
+        float yaw = BotSensors.gyro.getYaw();
+        double xOffset = Limelight.getXOffset();
+        m_targetAngle = yaw + xOffset;
+    }
+
+    @Override
+    public void execute() {
+        Limelight.calculateDistanceToTarget();
+
+        m_diffDriver.driveAlign(m_targetAngle);
+    }
+
+    // This finishes immediately, but is intended to be continually restarted while a button is held
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if (interrupted) {
+            System.out.println("--");
+            Logger.ending("Interrupting Command: RotateTowardsTarget...");
+        }
+    }
+
+}
