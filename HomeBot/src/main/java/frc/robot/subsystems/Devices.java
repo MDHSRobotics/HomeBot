31142eb01c82ec3bb3db6a085832bb299683d81c
Tyler Le <let1.21@stu.materdei.org>
2021-01-26 16:53:34 -0800

    Modified Devices ID for JV bot

--- HomeBot/src/main/java/frc/robot/subsystems/Devices.java
+++ HomeBot/src/main/java/frc/robot/subsystems/Devices.java
@@ -16,13 +16,21 @@ public class Devices {
     //////////////////////
 
     // TalonSRX
+<<<<<<< Updated upstream
     static DevTalonSRX talonSrxDiffWheelFrontLeft = new DevTalonSRX("talonFxDiffWheelFrontLeft", 12);
     static DevTalonSRX talonSrxDiffWheelFrontRight = new DevTalonSRX("talonFxDiffWheelFrontRight", 14);
     static DevTalonSRX talonSrxDiffWheelRearLeft = new DevTalonSRX("talonFxDiffWheelRearLeft", 4);
     static DevTalonSRX talonSrxDiffWheelRearRight = new DevTalonSRX("talonFxDiffWheelRearRight", 13);
+=======
+    static DevTalonSRX talonSrxDiffWheelFrontLeft = new DevTalonSRX("talonFxDiffWheelFrontLeft", 15);
+    static DevTalonSRX talonSrxDiffWheelFrontRight = new DevTalonSRX("talonFxDiffWheelFrontRight", 11);
+    static DevTalonSRX talonSrxDiffWheelRearLeft = new DevTalonSRX("talonFxDiffWheelRearLeft", 6);
+    static DevTalonSRX talonSrxDiffWheelRearRight = new DevTalonSRX("talonFxDiffWheelRearRight", 8);
+
     static DevTalonSRX talonSrxShooterBottomWheel = new DevTalonSRX("talonSrxShooterBottomWheel", 0);
     static DevTalonSRX talonSrxShooterTopWheel = new DevTalonSRX("talonSrxShooterTopWheel", 0);
     
+>>>>>>> Stashed changes
 
     /////////////////////
     // Drive Instances //
