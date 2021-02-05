4739a7c90a1442997c55bad7ef8486e16e416b27
Tyler Le <let1.21@stu.materdei.org>
2021-02-04 17:43:34 -0800

    Completed Delivery

---
+++ HomeBot/src/main/java/frc/robot/commands/Delivery/SpinDelivery.java
@@ -0,0 +1,50 @@
+
+package frc.robot.commands.Delivery;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.consoles.Logger;
+import frc.robot.subsystems.Delivery;
+
+// This command spins the Delivery
+public class SpinDelivery extends CommandBase {
+
+    
+    private Delivery m_Delivery;
+
+    public SpinDelivery(Delivery Delivery) {
+        Logger.setup("Constructing Command: SpinDelivery...");
+
+        // Add given subsystem requirements
+        m_Delivery = Delivery;
+        addRequirements(m_Delivery);
+
+    }
+
+    @Override
+    public void initialize() {
+        Logger.action("Initializing Command: SpinDeliveryAndConveyor...");
+    }
+
+    @Override
+    public void execute() {
+        m_Delivery.spin();
+    }
+
+    // This command continues until interrupted
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if (interrupted) {
+            System.out.println("--");
+            Logger.ending("Interrupting Command: SpinDeliveryAndConveyor...");
+        } else {
+            Logger.ending("Ending Command: SpinDeliveryAndConveyor...");
+        }
+    }
+
+}
