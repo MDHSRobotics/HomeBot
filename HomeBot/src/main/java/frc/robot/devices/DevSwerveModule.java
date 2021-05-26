fe784b6ec4a36b7d822f9df61bd095af9cf9048a
Tyler Le <let1.21@stu.materdei.org>
2021-05-26 12:30:33 -0700

    Minor reorganization and added sensor and motor phase config to modules

--- HomeBot/src/main/java/frc/robot/devices/DevSwerveModule.java
+++ HomeBot/src/main/java/frc/robot/devices/DevSwerveModule.java
@@ -7,27 +7,23 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
-import frc.robot.devices.DevTalonFX;
 import frc.robot.subsystems.SwerveDriver;
 import frc.robot.subsystems.utils.EncoderUtils;
 
 public class DevSwerveModule {
 
-    private static final double kModuleMaxAngularVelocity = SwerveDriver.kMaxAngularSpeed;
-    private static final double kModuleMaxAngularAcceleration = 2 * Math.PI; // radians per second squared
+    // Typical battery voltage
+    public static final double BATTERY_VOLTAGE = 12.0;
 
-    //talonFX motor controllers for drive and turn for each swerve module
-    private final DevTalonFX m_driveMotor;
-    private final DevTalonFX m_turningMotor;
-
-    //Typical battery voltage
-    public final double batteryVoltage = 12;
-
-    //translation objects that represent the respective distance a swerve module is to the center of the robot (in meters)
+    // Module objects
+    private final DevTalonFX m_driveTalon;
+    private final DevTalonFX m_turnTalon;
     private final Translation2d location;
-
-    private final PIDController m_drivePIDController = new PIDController(1, 0, 0);
     
+    // PID initialization
+    private static final double kModuleMaxAngularVelocity = SwerveDriver.kMaxAngularSpeed;  // radians per second squared
+    private static final double kModuleMaxAngularAcceleration = 2 * Math.PI;                // radians per second squared
+    private final PIDController m_drivePIDController = new PIDController(1, 0, 0);          // initialize PID profile
     private final ProfiledPIDController m_turningPIDController = new ProfiledPIDController(1, 0, 0, new TrapezoidProfile.Constraints(kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration));
     
     // Gains are for example purposes only - must be determined for your own robot!
@@ -35,20 +31,23 @@ public class DevSwerveModule {
     private final SimpleMotorFeedforward m_turnFeedforward = new SimpleMotorFeedforward(1, 0.5);
     
     /**
-     * Constructs a SwerveModule.
+     * Constructs a SwerveModule device.
      * 
-     * @param driveMotor talonFX for the drive motor.
-     * @param turnMotor talonFX for the turning motor.
-     * @param xpos horizontal position (in meters) from the center of the robot
-     * @param ypos vertical position (in meters) from the center of the robot
+     * @param driveTalon talonFX object of the drive motor
+     * @param turnTalon talonFX object of the turn motor
+     * @param xpos horizontal position of the module (in meters) from the center of the robot
+     * @param ypos vertical position of the module (in meters) from the center of the robot
      */
-    public DevSwerveModule(DevTalonFX driveMotor, DevTalonFX turnMotor, double xpos, double ypos) {
-        m_driveMotor = driveMotor;
-        m_turningMotor = turnMotor;
+    public DevSwerveModule(DevTalonFX driveTalon, DevTalonFX turnTalon, double xpos, double ypos, boolean SENSOR_PHASE, boolean MOTOR_INVERT) {
+        m_driveTalon = driveTalon;
+        m_turnTalon = turnTalon;
+
+        m_driveTalon.setSensorPhase(SENSOR_PHASE);
+        m_driveTalon.setInverted(MOTOR_INVERT);
+
         location = new Translation2d(xpos, ypos);
 
-        // Limit the PID Controller's input range between -pi and pi and set the input
-        // to be continuous.
+        // Limit the PID Controller's input range between -pi and pi and set the input to be continuous
         m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
     }
 
@@ -58,31 +57,38 @@ public class DevSwerveModule {
      * @param state Desired state with speed and angle.
      */
     public void setDesiredState(SwerveModuleState state) {
-        // Calculate the drive output from the drive PID controller.
-        final double driveOutput = m_drivePIDController.calculate(m_driveMotor.getSelectedSensorVelocity(), state.speedMetersPerSecond);
+        int driveVelocity = m_driveTalon.getSelectedSensorVelocity();
+        int turnPosition = m_turnTalon.getSelectedSensorPosition();
+        double turnPositionRadians = EncoderUtils.translateTicksToRadians(turnPosition);
 
-        final double driveFeedforward = m_driveFeedforward.calculate(state.speedMetersPerSecond);
+        double driveOutput = m_drivePIDController.calculate(driveVelocity, state.speedMetersPerSecond); // Calculate the drive output from the current velocity and a velocity setpoint
+        double driveFeedforward = m_driveFeedforward.calculate(state.speedMetersPerSecond); // Calculate the drive feed forward from a velocity setpoint
 
-        // Calculate the turning motor output from the turning PID controller.
-        final double turnOutput = m_turningPIDController.calculate(EncoderUtils.translateTicksToRadians(m_turningMotor.getSelectedSensorPosition()), state.angle.getRadians());
+        double turnOutput = m_turningPIDController.calculate(turnPositionRadians, state.angle.getRadians()); // Calculate the turning motor output from the current position and a position setpoint
+        double turnFeedforward = m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity); // Calculate the turn feed forward from a velocity setpoint
 
-        final double turnFeedforward = m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity);
-
-        //creates a percentage value for set by adding the voltages required for the respective motors and dividing by the current maximum battery voltage
-        m_driveMotor.set((driveOutput + driveFeedforward) / batteryVoltage);
-        m_turningMotor.set((turnOutput + turnFeedforward) / batteryVoltage);
+        // Creates a percentage value for set by adding the voltages required for the respective motors and dividing by the current maximum battery voltage
+        m_driveTalon.set((driveOutput + driveFeedforward) / BATTERY_VOLTAGE);
+        m_turnTalon.set((turnOutput + turnFeedforward) / BATTERY_VOLTAGE);
     }
 
     /**
      * Returns the current state of the module.
      *
-     * @return The current state of the module.
+     * @return The current state of the module
      */
-
     public SwerveModuleState getState() {
-        return new SwerveModuleState(m_driveMotor.getSelectedSensorVelocity(), new Rotation2d(EncoderUtils.translateTicksToRadians(m_turningMotor.getSelectedSensorPosition())));
+        int velocity = m_driveTalon.getSelectedSensorVelocity();
+        int position = m_turnTalon.getSelectedSensorPosition();
+        double positionRadians = EncoderUtils.translateTicksToRadians(position);
+        return new SwerveModuleState(velocity, new Rotation2d(positionRadians));
     }
 
+    /**
+     * Returns the location of the swerve module
+     * 
+     * @return The location of the swerve module
+     */
     public Translation2d getLocation() {
         return location;
     }
--- HomeBot/src/main/java/frc/robot/subsystems/Devices.java
+++ HomeBot/src/main/java/frc/robot/subsystems/Devices.java
@@ -25,15 +25,10 @@ public class Devices {
     public static DevTalonSRX talonSrxDeliveryRight = new DevTalonSRX("talonSrxDeliveryRight", 16);
     public static DevTalonSRX talonSrxDeliveryLeft = new DevTalonSRX("talonSrxDeliveryLeft", 7);
 
-    // DiffDriver
-    public static DevTalonFX talonFxDiffWheelFrontLeft = new DevTalonFX("talonFxDiffWheelFrontLeft", 23);
-    public static DevTalonFX talonFxDiffWheelFrontRight = new DevTalonFX("talonFxDiffWheelFrontRight", 20);
-    public static DevTalonFX talonFxDiffWheelRearLeft = new DevTalonFX("talonFxDiffWheelRearLeft", 24);
-    public static DevTalonFX talonFxDiffWheelRearRight = new DevTalonFX("talonFxDiffWheelRearRight", 21);
-
-    //Sensors
+    // Sensors
     public static Servo servoGate = new Servo(1);
 
+    // SwerveModule
     static DevTalonFX talonFxSwerveDriveWheelFrontLeft = new DevTalonFX("talonFxSwerveDriveWheelFrontLeft", 28);
     static DevTalonFX talonFxSwerveDriveWheelFrontRight = new DevTalonFX("talonFxSwerveDriveWheelFrontLeft", 26);
     static DevTalonFX talonFxSwerveDriveWheelRearLeft = new DevTalonFX("talonFxSwerveDriveWheelFrontLeft", 31);
@@ -46,16 +41,12 @@ public class Devices {
     /////////////////////
     // Drive Instances //
     /////////////////////
-
-    public static DevDifferentialDrive diffDriveTalonFX = new DevDifferentialDrive("diffDriveTalonFx",
-                                                                                talonFxDiffWheelFrontLeft,
-                                                                                talonFxDiffWheelFrontRight);
     
     //Swerve Modules
-    public static final DevSwerveModule frontLeftSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelFrontLeft, talonFxSwerveTurnWheelFrontLeft, 0.3155, 0.3155);
-    public static final DevSwerveModule frontRightSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelFrontRight, talonFxSwerveTurnWheelFrontRight, 0.3155, 0.3155);
-    public static final DevSwerveModule rearLeftSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelRearLeft, talonFxSwerveTurnWheelRearLeft, 0.3155, 0.3155);
-    public static final DevSwerveModule rearRightSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelRearRight, talonFxSwerveTurnWheelRearRight, 0.3155, 0.3155);
+    public static final DevSwerveModule frontLeftSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelFrontLeft, talonFxSwerveTurnWheelFrontLeft, 0.3155, 0.3155, true, true);
+    public static final DevSwerveModule frontRightSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelFrontRight, talonFxSwerveTurnWheelFrontRight, 0.3155, 0.3155, true, true);
+    public static final DevSwerveModule rearLeftSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelRearLeft, talonFxSwerveTurnWheelRearLeft, 0.3155, 0.3155, true, true);
+    public static final DevSwerveModule rearRightSwerveModule = new DevSwerveModule(talonFxSwerveDriveWheelRearRight, talonFxSwerveTurnWheelRearRight, 0.3155, 0.3155, true, true);
 
     
 }
