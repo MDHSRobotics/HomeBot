7b1af6bf1610523a07142ce6647892aee826f640
Kevin <tranh.22@stu.materdei.org>
2021-06-01 17:23:13 -0700

    Setting up motor for the flywheel and the hood

---
+++ HomeBot/src/main/java/frc/robot/subsystems/HoodShooter.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.brains.LimelightBrain;
+import frc.robot.brains.ShooterBrain;
+import frc.robot.consoles.Logger;
+import frc.robot.subsystems.utils.*;
+import static frc.robot.subsystems.Devices.talonSrxShooterHood;
+import static frc.robot.RobotManager.isReal;
+public class HoodShooter extends SubsystemBase{
+    
+}
