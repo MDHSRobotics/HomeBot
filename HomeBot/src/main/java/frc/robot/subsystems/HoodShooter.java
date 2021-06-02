647bf31d7f860b4f45e253b20d0f04a5b9af90d1
Kevin <tranh.22@stu.materdei.org>
2021-06-01 17:24:01 -0700

    Setting up motor for the flywheel and the hood

--- HomeBot/src/main/java/frc/robot/subsystems/HoodShooter.java
+++ HomeBot/src/main/java/frc/robot/subsystems/HoodShooter.java
@@ -7,6 +7,27 @@ import frc.robot.consoles.Logger;
 import frc.robot.subsystems.utils.*;
 import static frc.robot.subsystems.Devices.talonSrxShooterHood;
 import static frc.robot.RobotManager.isReal;
-public class HoodShooter extends SubsystemBase{
+public class HoodShooter extends SubsystemBase {
+    // Encoder constants
+    private static final boolean SENSOR_PHASE_HOOD = true;
+    private static final boolean MOTOR_INVERT_HOOD = false;
+
+    private static final boolean SENSOR_PHASE_FLYWHEEL = true;
+    private static final boolean MOTOR_INVERT_FLYWHEEL = false;
+
+    // Position constants
+    private static final double GEAR_RATIO = 4.0; // Gear ratio bewteen Input Shaft : Output Shaft of a gearbox
     
-}
+     public HoodShooter(){
+       if(isReal){
+        PIDValues pidHood = new PIDValues(0.0, 0.0, 0.0, 0.0);
+        TalonUtils.configureTalonWithEncoder(talonSrxShooterHood, SENSOR_PHASE_HOOD, MOTOR_INVERT_HOOD, pidHood);
+       
+        PIDValues pidShooter = new PIDValues(0.00835, 0.0, 0.0, 0.0); // Calibrated for 20,000: TpHMS 0.00835
+        TalonUtils.configureTalonWithEncoder(talonSrxShooterTopWheel, SENSOR_PHASE_FLYWHEEL, MOTOR_INVERT_FLYWHEEL, pidShooter);
+       
+      } 
+    }
+      
+
+}     
