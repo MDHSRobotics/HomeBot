ea8ca9d00486d2a6895adab85ada24c336d6abf8
John Cardullo <igsgame@outlook.com>
2021-09-28 15:23:52 -0700

    Updated DIY Swerve
    I did all of it. Kenneth is useless

--- HomeBot/src/main/java/frc/robot/devices/DevSwerveModule.java
+++ HomeBot/src/main/java/frc/robot/devices/DevSwerveModule.java
@@ -19,87 +19,23 @@ public class DevSwerveModule {
     // Accessible module objects
     private final DevTalonFX m_driveTalon;
     private final DevTalonFX m_steerTalon;
-    private final Translation2d location;
     
     // PID initialization
     private static final double kModuleMaxAngularVelocity = 0.5;//SwerveDriver.kMaxAngularSpeed;  // radians per second squared
     private static final double kModuleMaxAngularAcceleration = 0.5;//2 * Math.PI;                // radians per second squared
-    private final PIDController m_drivePIDController = new PIDController(0.87, 0, 0);          // initialize PID profile
-    private final ProfiledPIDController m_turningPIDController = new ProfiledPIDController(0.87, 0, 0, new TrapezoidProfile.Constraints(kModuleMaxAngularVelocity, kModuleMaxAngularAcceleration));
-    
-    // Configure feed forward gains (experimentally found)
-    private final SimpleMotorFeedforward m_driveFeedforward = new SimpleMotorFeedforward(0, 0.45);
-    private final SimpleMotorFeedforward m_turnFeedforward = new SimpleMotorFeedforward(0, 0.45);
     
     /**
      * Constructs a SwerveModule device.
      * 
      * @param driveTalon talonFX object of the drive motor
      * @param steerTalon talonFX object of the steer motor
-     * @param xpos horizontal position of the module (in meters) from the center of the robot
-     * @param ypos vertical position of the module (in meters) from the center of the robot
      */
-    public DevSwerveModule(DevTalonFX driveTalon, DevTalonFX steerTalon, double xpos, double ypos, boolean SENSOR_PHASE, boolean MOTOR_INVERT) {
+    public DevSwerveModule(DevTalonFX driveTalon, DevTalonFX steerTalon, boolean SENSOR_PHASE, boolean MOTOR_INVERT) {
         m_driveTalon = driveTalon;
         m_steerTalon = steerTalon;
-        location = new Translation2d(xpos, ypos);
 
         m_driveTalon.setSensorPhase(SENSOR_PHASE);
         m_driveTalon.setInverted(MOTOR_INVERT);
-
-        // Limit the PID Controller's input range between -pi and pi and set the input to be continuous
-        m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
-    }
-
-    /**
-     * Sets the desired state for the module.
-     * 
-     * @param state Desired state with speed and angle.
-     */
-    public void setDesiredState(SwerveModuleState state) {
-        int driveVelocity = (int) m_driveTalon.getSelectedSensorVelocity();
-        int turnPosition = (int) m_steerTalon.getSelectedSensorPosition();
-        double turnPositionRadians = EncoderUtils.translateTicksToRadians(turnPosition);
-
-        double driveOutput = m_drivePIDController.calculate(EncoderUtils.translateTicksToDistance(driveVelocity * 3.048, 4 * Math.PI), state.speedMetersPerSecond); // Calculate the drive output from the current velocity and a velocity setpoint
-        //convert from feet per 100 ms to meters per second
-        double driveFeedforward = m_driveFeedforward.calculate(state.speedMetersPerSecond); // Calculate the drive feed forward from a velocity setpoint
-
-        double turnOutput = m_turningPIDController.calculate(turnPositionRadians, state.angle.getRadians()); // Calculate the turning motor output from the current position and a position setpoint
-        double turnFeedforward = m_turnFeedforward.calculate(m_turningPIDController.getSetpoint().velocity); // Calculate the turn feed forward from a velocity setpoint
-
-        // Creates a percentage value for set by adding the voltages required for the respective motors and dividing by the current maximum battery voltage
-        double driveTalonVoltage = (driveOutput + driveFeedforward) / BATTERY_VOLTAGE;
-        double steerTalonVoltage = (turnOutput + turnFeedforward) / BATTERY_VOLTAGE;
-        Logger.info("Output: " + driveOutput);
-        Logger.info("Output: " + turnOutput);
-        Logger.info("Feed: " + driveFeedforward);
-        Logger.info("Feed: " + turnFeedforward);
-        Logger.info("Voltage: " + driveTalonVoltage);
-        Logger.info("Voltage: " + steerTalonVoltage);
-        m_driveTalon.set(driveTalonVoltage);
-        m_steerTalon.set(steerTalonVoltage);
-    }
-
-    /**
-     * Gets the current state of the module.
-     *
-     * @return The current state of the module
-     */
-    public SwerveModuleState getCurrentState() {
-        int velocity = (int) m_driveTalon.getSelectedSensorVelocity();
-        int position = (int) m_steerTalon.getSelectedSensorPosition();
-        double positionRadians = EncoderUtils.translateTicksToRadians(position);
-        return new SwerveModuleState(velocity, new Rotation2d(positionRadians));
-    }
-
-    /**
-     * Returns the location of the swerve module
-     * 
-     * @return The location of the swerve module
-     */
-    public Translation2d getLocation() {
-        return location;
     }
 
     public void stopModule(){
--- HomeBot/src/main/java/frc/robot/subsystems/SwerveDriver.java
+++ HomeBot/src/main/java/frc/robot/subsystems/SwerveDriver.java
@@ -11,74 +11,17 @@ import frc.robot.BotSensors;
 import frc.robot.consoles.Logger;
 
 public class SwerveDriver extends SubsystemBase {
-    public static final double kMaxSpeed = 1.0; // 1 meter per second
-    public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
 
-    // Control if you want to move from the point of view of the robot or from the point of view of the field
-    public static final boolean fieldRelative = false;
     
     // Control whether you want the thumbstick axes to be flipped in the opposite direction.
     public static final boolean isYFlipped = false;
     public static final boolean isXFlipped = false;
     public static final boolean isOmegaFlipped = false;
 
-    // The kinematics object is created from the locations of the swerve modules
-    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(Devices.frontLeftSwerveModule.getLocation(), Devices.frontRightSwerveModule.getLocation(), Devices.rearLeftSwerveModule.getLocation(), Devices.rearRightSwerveModule.getLocation());
-
-    // The odometry object takes the kinematics object and the current angle of the robot (the odometry should be updated)
-    private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics, getAngle());
-
-    public SwerveDriver() {
-        BotSensors.gyro.reset();
-    }
-
-    // Returns the current gyro angle
-    public Rotation2d getAngle() {
-        return Rotation2d.fromDegrees(-BotSensors.gyro.getAngle()); // Flipping the angle because WPILib gyros are CW positive.
-    }
-
     // Drive using a vertical, horizontal, and rotational velocity
     public void drive(double xVel, double yVel, double omega) {
-        double m_xVel = xVel;
-        double m_yVel = yVel;
-        double m_omega = omega;
-        // ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(0, 1, 0, Rotation2d.fromDegrees(0));
-        SwerveModuleState[] swerveModuleStates; // = m_kinematics.toSwerveModuleStates(speeds);
-
-        // Flip axis directions 
-        if (isXFlipped) {
-            m_xVel = -xVel;
-        }
-        if (isOmegaFlipped) {
-            m_omega = -omega;
-        }
-
-        // Choose between robot or field relative driving
-        if (fieldRelative) {
-            swerveModuleStates = m_kinematics.toSwerveModuleStates(ChassisSpeeds.fromFieldRelativeSpeeds(m_xVel, m_yVel, m_omega, getAngle()));
-        } 
-        else {
-            swerveModuleStates = m_kinematics.toSwerveModuleStates(new ChassisSpeeds(m_xVel, m_yVel, m_omega));
-        }
-
-        //SwerveDriveKinematics.normalizeWheelSpeeds(swerveModuleStates, kMaxSpeed);
-
-        // Set the state of each swerve module in order to achieve the specified drive velocities
-        
-        Devices.frontLeftSwerveModule.setDesiredState(swerveModuleStates[0]);
-        Devices.frontRightSwerveModule.setDesiredState(swerveModuleStates[1]);
-        Devices.rearLeftSwerveModule.setDesiredState(swerveModuleStates[2]);
-        Devices.rearRightSwerveModule.setDesiredState(swerveModuleStates[3]);
-
-        Logger.info(swerveModuleStates[0].toString());
     }
 
-    // The odometry object updates its position given a current gyro angle and current module states
-    public void updateOdometry() {
-        Pose2d pose = m_odometry.update(getAngle(), Devices.frontLeftSwerveModule.getCurrentState(), Devices.frontRightSwerveModule.getCurrentState(), Devices.rearLeftSwerveModule.getCurrentState(), Devices.rearRightSwerveModule.getCurrentState());
-        //Logger.info(pose.toString());
-    }
-    
     // Stop all the swerve modules
     public void stop() {
         Devices.frontLeftSwerveModule.stopModule();
