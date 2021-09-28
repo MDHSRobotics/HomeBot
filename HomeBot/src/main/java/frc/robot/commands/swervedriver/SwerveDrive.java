328aa3eb6c49ddc07dac04b02cdd1562247014fd
Lucas Frayne <fraynel.22@stu.materdei.org>
2021-09-28 16:53:28 -0700

    Deleted unnecessary import statements in the SwerveDrive subsystem

--- HomeBot/src/main/java/frc/robot/commands/swervedriver/SwerveDrive.java
+++ HomeBot/src/main/java/frc/robot/commands/swervedriver/SwerveDrive.java
@@ -34,28 +34,7 @@ public class SwerveDrive extends CommandBase {
         // m_swerveDriver.testMotors();
     }
 
-    public final double L = .47;
-    public final double W = .47;
-
-    public void drive (double x1, double y1, double x2) {
-        double r = Math.sqrt ((L * L) + (W * W));
-        y1 *= -1;
-
-        double a = x1 - x2 * (L / r);
-        double b = x1 + x2 * (L / r);
-        double c = y1 - x2 * (W / r);
-        double d = y1 + x2 * (W / r);
-
-        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
-        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
-        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
-        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
-
-        double backRightAngle = Math.atan2 (a, d) / Math.PI;
-        double backLeftAngle = Math.atan2 (a, c) / Math.PI;
-        double frontRightAngle = Math.atan2 (b, d) / Math.PI;
-        double frontLeftAngle = Math.atan2 (b, c) / Math.PI;
-    }
+    
 
     // This command continues until interrupted.
     @Override
--- HomeBot/src/main/java/frc/robot/subsystems/SwerveDriver.java
+++ HomeBot/src/main/java/frc/robot/subsystems/SwerveDriver.java
@@ -1,14 +1,9 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
-import frc.robot.BotSensors;
-import frc.robot.consoles.Logger;
+
+import static frc.robot.subsystems.Devices.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class SwerveDriver extends SubsystemBase {
 
@@ -19,7 +14,42 @@ public class SwerveDriver extends SubsystemBase {
     public static final boolean isOmegaFlipped = false;
 
     // Drive using a vertical, horizontal, and rotational velocity
-    public void drive(double xVel, double yVel, double omega) {
+    public final double L = .47;
+    public final double W = .47;
+
+    public void drive (double x1, double y1, double x2) {
+        double r = Math.sqrt ((L * L) + (W * W));
+        y1 *= -1;
+
+        double a = x1 - x2 * (L / r);
+        double b = x1 + x2 * (L / r);
+        double c = y1 - x2 * (W / r);
+        double d = y1 + x2 * (W / r);
+
+        double backRightSpeed = Math.sqrt ((a * a) + (d * d));
+        double backLeftSpeed = Math.sqrt ((a * a) + (c * c));
+        double frontRightSpeed = Math.sqrt ((b * b) + (d * d));
+        double frontLeftSpeed = Math.sqrt ((b * b) + (c * c));
+
+        double backRightAngle = Math.atan2 (a, d) / Math.PI;
+        double backLeftAngle = Math.atan2 (a, c) / Math.PI;
+        double frontRightAngle = Math.atan2 (b, d) / Math.PI;
+        double frontLeftAngle = Math.atan2 (b, c) / Math.PI;
+
+        int tickBackRightAngle = (int) (backRightAngle * 4096);
+        int tickBackLeftAngle = (int) (backLeftAngle * 4096);
+        int tickFrontRightAngle = (int) (frontRightAngle * 4096);
+        int tickFrontLeftAngle = (int) (frontLeftAngle * 4096);
+
+        talonFxSwerveDriveWheelRearRight.set(backRightSpeed);
+        talonFxSwerveDriveWheelRearLeft.set(backLeftSpeed);
+        talonFxSwerveDriveWheelFrontRight.set(frontRightSpeed);
+        talonFxSwerveDriveWheelFrontLeft.set(frontLeftSpeed);
+
+        talonFxSwerveTurnWheelFrontLeft.set(ControlMode.Position, tickFrontLeftAngle);
+        talonFxSwerveTurnWheelFrontRight.set(ControlMode.Position, tickFrontRightAngle);
+        talonFxSwerveTurnWheelRearLeft.set(ControlMode.Position, tickBackLeftAngle);
+        talonFxSwerveTurnWheelRearRight.set(ControlMode.Position, tickBackRightAngle);
     }
 
     // Stop all the swerve modules
