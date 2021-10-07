545a944df9c0e2a77528567b244a93dc3369b1ee
Kent Wilkison <wilkisonk.23@stu.materdei.org>
2021-10-07 16:47:54 -0700

    Updated Path Constants

--- HomeBot/src/main/java/frc/robot/subsystems/constants/PathConstants.java
+++ HomeBot/src/main/java/frc/robot/subsystems/constants/PathConstants.java
@@ -14,17 +14,17 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
  */
 public final class PathConstants {
 
-    public static final int kLeftMotor1Port = 0;
-    public static final int kLeftMotor2Port = 1;
-    public static final int kRightMotor1Port = 2;
-    public static final int kRightMotor2Port = 3;
+    public static final int kLeftMotor1Port = 20;
+    public static final int kLeftMotor2Port = 21;
+    public static final int kRightMotor1Port = 23;
+    public static final int kRightMotor2Port = 24;
 
-    public static final int[] kLeftEncoderPorts = new int[] { 0, 1 };
-    public static final int[] kRightEncoderPorts = new int[] { 2, 3 };
+    public static final int[] kLeftEncoderPorts = new int[] { 20, 21 };
+    public static final int[] kRightEncoderPorts = new int[] { 23, 24 };
     public static final boolean kLeftEncoderReversed = false;
     public static final boolean kRightEncoderReversed = true;
 
-    public static final double kTrackwidthMeters = .6096;
+    public static final double kTrackwidthMeters = 0.8485427401417107;
     public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
 
     public static final int kEncoderCPR = 2048;
@@ -42,12 +42,13 @@ public final class PathConstants {
     // The Robot Characterization Toolsuite provides a convenient tool for obtaining
     // these
     // values for your robot.
-    public static final double ksVolts = 0;
-    public static final double kvVoltSecondsPerMeter = 0;
-    public static final double kaVoltSecondsSquaredPerMeter = 0;
+    public static final double ksVolts = 0.717;
+    public static final double kvVoltSecondsPerMeter = 0.891;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.215;
 
     // Example value only - as above, this must be tuned for your drive!
-    public static final double kPDriveVel = 0.004033698535276000002;
+    public static final double kPDriveVel = 0.052;
+    //0.004033698535276000002;
     //smallest fast:
     //lowest
     public static final class OIConstants {
